ede182884fd905b9bac74b1e35985b6c6dda2f88
aprozo <alexandr.a.prozorov@gmail.com>
2024-08-23 15:12:33 +0200

    cosmetics

--- app.py
+++ app.py
@@ -1,27 +1,18 @@
 import streamlit as st
-from langchain.schema import StrOutputParser
-from langchain_core.runnables import  RunnablePassthrough
-from langchain.schema.runnable import RunnableMap
-from operator import itemgetter
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
-
-from langchain.retrievers import ParentDocumentRetriever
 from langchain_community.embeddings.sentence_transformer import SentenceTransformerEmbeddings
-
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain.storage import LocalFileStore
-from langchain.storage._lc_store import create_kv_docstore
-
 from pinecone.grpc import PineconeGRPC as Pinecone
 from langchain_pinecone import PineconeVectorStore
-
-from langchain.callbacks.base import BaseCallbackHandler
-from langchain.schema import ChatMessage
-
+import os
+from langchain_community.chat_message_histories import StreamlitChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever,create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.runnables.history import RunnableWithMessageHistory
 from dotenv import load_dotenv
 load_dotenv()
-import os
 
 st.set_page_config(page_title="STAR chat", page_icon=":star:")
 st.image("STAR-logo-trans.gif")
@@ -52,18 +43,15 @@ with st.sidebar:
 if useParentDocument:
     database="Chroma (Local)"
 
-
 pc = Pinecone(api_key=st.secrets["pinecone_api_key"])
 index_name = "arxiv-papers-md"
 index = pc.Index(index_name)
-
 
 if database == "Chroma (Local)":
     st.warning("switch to cloud")
 else:
     vectorestore = PineconeVectorStore(index_name=index_name, embedding=embedding_function, pinecone_api_key = st.secrets["pinecone_api_key"])
     retriever = vectorestore.as_retriever(search_kwargs={"k": top_k})    
-
 
 openai_api_key=st.secrets["openai_api_key"]
 os.environ["LANGSMITH_API_KEY"] = st.secrets["langsmith_api_key"]
@@ -75,15 +63,15 @@ system_prompt  = """
 You are an expert on the STAR experiment, a high-energy nuclear physics experiment at the Relativistic Heavy Ion Collider (RHIC) at Brookhaven National Laboratory. \
 Your task is to answer questions specifically related to the STAR experiment, its findings, technologies, and related topics.  \
 Refrain any other topics by saying you will not answer questions about them and Exit right away here. DO NOT PROCEED. \
-You are not allowed to use any other sources other than the provided search results. \
+You are not allowed to use any other sources other than the provided search results and chat history. \
 
 Generate a comprehensive, and informative answer strictly within 200 words or less for the \
-given question based solely on the provided search results (URL and content). You must \
-only use information from the provided search results. Use an unbiased and \
+given question based solely on the provided search results (URL and content) and chat history. You must \
+only use information from the provided search results and chat history. Use an unbiased and \
 journalistic tone. Combine search results together into a coherent answer. Do not \
 repeat text. You should use bullet points in your answer for readability. Make sure to break down your answer into bullet points.\
-You should not hallicunate nor build up any references, Use only the `context` html block below and do not use any text within <ARXIV_ID> and </ARXIV_ID> except when citing in the end.  \
-Make sure not to repeat the same context. Be specific to the exact question asked for.\
+You should not hallicunate nor build up any references, do not use any text within <ARXIV_ID> and </ARXIV_ID> except when citing in the end.  \
+Make sure not to repeat the same context. Be specific to the exact questions. Take you time.\
 
 Here is the response template:
 ---
@@ -120,10 +108,10 @@ I'm not sure." or "This is beyond the chat scope" or greet back or politely refu
 Make sure to highlight the most important key words in bold font. Don't repeat any context nor points in the answer.\
 
 Anything between the following `context`  html blocks is retrieved from a knowledge \
-bank, not part of the conversation with the user. The context are numbered based on its knowledge retrival and increasing cosine similarity index. \
-Make sure to consider the order in which they appear context appear. It is an increasing order of cosine similarity index.\
+bank. The context is numbered based on its knowledge retrival and increasing cosine similarity index. \
+Make sure to consider the order in which context appear. It is an increasing order of cosine similarity index.\
 The contents are formatted in latex, you need to remove any special characters and latex formatting before cohercing the points to build your answer.\
-Write your answer in the form of markdown bullet points. You can use latex commands if necessary.
+You can use latex commands if necessary.\
 You will strictly cite no more than 10 unqiue citations at maximum from the context below.\
 Make sure these citations have to be relavant and strictly do not repeat the context in the answer.
 
@@ -131,21 +119,10 @@ Make sure these citations have to be relavant and strictly do not repeat the con
     {context}
 <context/>
 
-REMEMBER: If there is no relevant information within the context, just say "Hmm, I'm \
-not sure." or greet back. Don't try to make up an answer. Anything between the preceding 'context' \
-html blocks is retrieved from a knowledge bank, not part of the conversation with the \
-user.\
+REMEMBER: If there is no relevant information within the context or chat history, just say "Hmm, I'm \
+not sure." or greet back. Don't try to make up an answer.\
 Question: {input}
 """
-llm = ChatOpenAI(model_name="gpt-4o-mini", streaming=True, openai_api_key=openai_api_key)
-
-from langchain_community.chat_message_histories import StreamlitChatMessageHistory
-from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
-
-from langchain.chains import create_retrieval_chain
-from langchain.chains import create_history_aware_retriever
-from langchain.chains.combine_documents import create_stuff_documents_chain
-
 
 ### Contextualize question ###
 contextualize_q_system_prompt = (
@@ -164,10 +141,10 @@ contextualize_q_prompt = ChatPromptTemplate.from_messages(
     ]
 )
 contextualize_q_llm = llm.with_config(tags=["contextualize_q_llm"])
+
 history_aware_retriever = create_history_aware_retriever(
     contextualize_q_llm, retriever, contextualize_q_prompt
 )
-
 qa_prompt = ChatPromptTemplate.from_messages(
     [
         ("system", system_prompt),
@@ -175,26 +152,18 @@ qa_prompt = ChatPromptTemplate.from_messages(
         ("human", "{input}"),
     ]
 )
-
-from langchain_core.prompts import PromptTemplate
-
 custom_document_prompt = PromptTemplate(
     input_variables=["page_content", "title" , "arxiv_id"],  # Replace "your_variable_name" with your actual variable name
-    template="Title:{title}, <ARXIV_ID>{arxiv_id}<ARXIV_ID/>\n Text:{page_content}"  # Customize your template
+    template="Title:{title} \n <ARXIV_ID>{arxiv_id}<ARXIV_ID/>\n Text:{page_content}"  # Customize your template
 )
-
 question_answer_chain = create_stuff_documents_chain(
                                                     llm, qa_prompt,
                                                     document_prompt = custom_document_prompt)
 
 rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
-
-
-from langchain_core.runnables.history import RunnableWithMessageHistory
 msgs = StreamlitChatMessageHistory(key="langchain_messages")
 if len(msgs.messages) == 0:
     msgs.add_ai_message("How can I help you?")
-
 
 conversational_rag_chain = RunnableWithMessageHistory(
     rag_chain,
@@ -203,8 +172,6 @@ conversational_rag_chain = RunnableWithMessageHistory(
     history_messages_key="chat_history",
     output_messages_key="answer",
 )
-
-
 # Render current messages from StreamlitChatMessageHistory
 for msg in msgs.messages:
     st.chat_message(msg.type).write(msg.content)
@@ -217,5 +184,3 @@ if input_question := st.chat_input("Ask a question, e.g. What are TPC operationa
     with st.spinner("Retrieving information..."):
             response = conversational_rag_chain.invoke({"input": input_question}, config)
     st.chat_message("ai").write(response["answer"])
-
-
